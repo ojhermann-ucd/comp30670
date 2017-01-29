daf8b20b3a4db37d229b45bfd52dd600ea4876f6
ojh <ojh@dhcp-892b9e7f.ucd.ie>
2017-01-29 22:41:49 +0000

    Signed-off-by: ojh <ojh@dhcp-892b9e7f.ucd.ie>

---
+++ systeminfo/systeminfo/main.py
@@ -0,0 +1,19 @@
+"""
+created
+2017 01 29
+
+author
+ojh
+
+purpose
+complete an assignment
+"""
+
+import platform
+
+def main():
+    print(platform.platform())
+    return
+
+if __name__ == '__main__':
+    main()
